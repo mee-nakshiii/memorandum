4c1500a504276fed9dd1667a174f538bcc5172c5
mee-nakshiii <meenakshisudheesh@gmail.com>
2026-06-30 09:53:26 +0530

    API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "http://localhost:5000/api"
+const api = axios.create({
+    baseURL: "https://your-backend-url.com/api",
 });
